c0535feaa59f18b00d6e9ffcbef527e2a253bd1a
appleyib <appleyibwayne@gmail.com>
2019-12-03 19:31:08 -0800

    current change

--- Model.py
+++ Model.py
@@ -3,10 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-collabSim = np.load("collabSim.npy")
-contentSim = np.load("contentSim.npy")
-nLarge = np.load("nLarge.npy")
-
 
 class ContentBased(object):
 
@@ -54,13 +50,11 @@ class HybridBased(object):
         a_2 = np.array(self.collab.loc[query]).reshape(1, -1)
         content = cosine_similarity(self.content, a_1).reshape(-1)
         collaborative = cosine_similarity(self.collab, a_2).reshape(-1)
-        curContentSim = contentSim[self.content.loc(query)]
-        curCollabSim = collabSim[self.collab.loc(query)]
         hybrid = ((content + collaborative)/2.0)
         # a data frame of movies based on similarity to query
         dictDf = {'hybrid': hybrid}
         similar = pd.DataFrame(dictDf, index=self.content.index)
         # similar.sort_values('hybrid', ascending=False, inplace=True)
         # sorting is too in efficiency, we can use pandas.Dataframe.nsmallest to find n most simillar dataframe
-        similar = similar.nsmallest(n+1, 'hybrid', keep='all')
+        similar = nsmallest(n+1, 'hybrid', keep='all')
         return similar.head(n+1)[1:].index.tolist()
--- preCalculate.py
+++ preCalculate.py
@@ -6,45 +6,48 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 index = None
 
+# calculates the cosine similarity of between each pair of data
 def pre_calculate_cosine_sim(fileP):
 	data = None
 	with open(fileP, 'rb') as f:
 		data = pickle.load(f)
 	if data is not None:
-		index = data.index
 		data_sim = cosine_similarity(data, data)
 	else:
 		print("No collaborative pkl file!")
 	return data_sim
 
-def pre_calculate_n_smallest(data, n):
-	# dictDf = {'data': data}
- #    dataPd = pd.DataFrame(dictDf, index=index)
- #    nLarge = dataPd.nlargest(n+1, 'data')
+# calculates the n movies with largest similarity for each movie (except its own)
+def pre_calculate_n_largest(data, n):
 	l = data.shape[0]
-	nLarge = np.array([(-data[i]).argpartition(n+1)[1:n+1] for i in range(l)])
+	nLarge = np.array([(-data[i]).argpartition(n)[:n] for i in range(l)])
 	print("Shape of the n most similar movies to each movie is:", nLarge.shape)
 	print(nLarge)
 	return nLarge
 
+# sets up variables
 i=1
 collab_cosine_sim = None
 content_cosine_sim = None
 
+# arguments defines whether we use collaborative feature or we use content feature or both
+# uses collab feat
 if len(sys.argv) >= i+2 and sys.argv[i] == "-collab":
 	collab_cosine_sim = pre_calculate_cosine_sim(sys.argv[i+1])
-	outFile = open("collabSim.npy", "wb")
-	np.save(outFile, collab_cosine_sim)
+	# outFile = open("collabSim.npy", "wb")
+	# np.save(outFile, collab_cosine_sim)
 	i += 2
 	print("Collab consine cosine_similarity calculation complete!")
 
+# uses content feat
 if len(sys.argv) >= i+2 and sys.argv[i] == "-content":
 	content_cosine_sim = pre_calculate_cosine_sim(sys.argv[i+1])
-	outFile = open("contentSim.npy", "wb")
-	np.save(outFile, content_cosine_sim)
+	# outFile = open("contentSim.npy", "wb")
+	# np.save(outFile, content_cosine_sim)
 	i += 2
 	print("Content consine cosine_similarity calculation complete!")
 
+# the variable to store the final similarity
 sim = None
 if collab_cosine_sim is None:
 	sim = content_cosine_sim
@@ -52,8 +55,10 @@ elif content_cosine_sim is None:
 	sim = collab_cosine_sim
 else:
 	sim = content_cosine_sim + collab_cosine_sim
-nLarge = pre_calculate_n_smallest(sim, 10)
+
+nLarge = pre_calculate_n_largest(sim, 10)
 outFile = open("nLarge.npy", "wb")
 np.save(outFile, nLarge)
 print("N largest sim for each movie calculation complete!")
+print("Pre calculation done!")
 
